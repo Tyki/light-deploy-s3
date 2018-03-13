6795680cfa7ba559d56105e87f1d622b3fd0781d
Baptiste Leulliette <bleulliette@kaliop.com>
2018-03-13 10:31:05 +0100

    Complete refactor of directory upload

--- deploy.js
+++ deploy.js
@@ -1,14 +1,14 @@
-var s3 = require('s3')
-var AWS = require('aws-sdk')
-var fs = require('fs')
-var path = require('path')
-var argv = require('minimist')(process.argv.slice(2))
-var ora = require('ora')
-var dotenv = require('dotenv')
+const s3 = require('s3')
+const AWS = require('aws-sdk')
+const fs = require('fs')
+const path = require('path')
+const argv = require('minimist')(process.argv.slice(2))
+const ora = require('ora')
+const dotenv = require('dotenv')
 
-var config = dotenv.config().parsed
+const config = dotenv.config().parsed
 
-var allowedACL = ['private', 'public-read', 'public-read-write', 'authenticated-read', 'aws-exec-read', 'bucket-owner-read', 'bucker-owner-full-control']
+const allowedACL = ['private', 'public-read', 'public-read-write', 'authenticated-read', 'aws-exec-read', 'bucket-owner-read', 'bucker-owner-full-control']
 
 if (!config) {
     console.error('Missing .env file')
@@ -25,13 +25,13 @@ if (!config.hasOwnProperty('aws_secret_access_key')) {
     process.exit(1)    
 }
 
-var accessKey = config.aws_access_key_id
-var secretKey = config.aws_secret_access_key
+const accessKey = config.aws_access_key_id
+const secretKey = config.aws_secret_access_key
 
-var bucketName = ''
-var bucketRegion = ''
-var file = ''
-var cwd = ''
+let bucketName = ''
+let bucketRegion = ''
+let baseFilePath = ''
+let cwd = ''
 
 var allParametersRequired = true
 var mandatoryParameters = ['file', 'bucket', 'region', 'cwd']
@@ -42,7 +42,7 @@ mandatoryParameters.forEach(parameter => {
     }
 })
 
-var ACL = 'public-read'
+const ACL = 'public-read'
 if (argv.hasOwnProperty('ACL')) {
     if (allowedACL.indexOf(argv.ACL) !== -1) {
         ACL = argv.ACL
@@ -56,16 +56,25 @@ if (!allParametersRequired) {
     process.exit(1)
 }
 
-file = path.resolve(argv.file)
+baseFilePath = path.resolve(argv.file)
 bucketName = argv.bucket
 bucketRegion = argv.region
 cwd = argv.cwd
+
+const awsS3Client = new AWS.S3({
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
+    region: bucketRegion
+})
+const client = s3.createClient({
+    s3Client: awsS3Client
+})
 
 console.log(`Starting the deployment to S3 bucket : ${bucketName} (${bucketRegion})`)
 
 var isDir = false
 var fileName = ''
-fs.open(file, 'r', (error, fd) => {
+fs.open(baseFilePath, 'r', (error, fd) => {
     if (error) {
         console.error('Error reading the file or directory')
         process.exit(1)
@@ -79,71 +88,136 @@ fs.open(file, 'r', (error, fd) => {
 
         isDir = stat.isDirectory()
         if (!isDir) {
-            fileName = path.basename(file)
+            var spinner = ora('Start uploading')
+            spinner.start()
+    
+            startUpload(filePath)
+            .then(() => {
+                spinner.stop()
+                process.exit(0)
+            })
+            .catch(error => {
+                console.error(error)
+                process.exit(1)
+            })
+
+        } else {
+            var files = walkSync(baseFilePath)
+            
+
+            processUploadDir(files, null)
+            .then(() => {
+                process.exit(0)
+            })
+            .catch(error => {
+                console.error(error)
+                process.exit(1)
+            })
         }
+
         fs.close(fd, (error) => {
             if (error) {
                 console.error('Error closing the file descriptor')
                 process.exit(1)
             }
 
-            startUpload()
         })
     })
 })
 
 /**
  * Upload file or directory content to specified bucket name and region
+ * 
+ * @param {String} filepath - File path of the file to uploader
+ * @returns {Promise}
  */
-function startUpload () {
+const startUpload = (filepath, fromDirectory) => {
+    return new Promise((resolve, reject) => {
+        var fileName = path.basename(filepath)
 
-    var awsS3Client = new AWS.S3({
-        accessKeyId: accessKey,
-        secretAccessKey: secretKey,
-        region: bucketRegion
-    })
-    var client = s3.createClient({
-        s3Client: awsS3Client
-    })
+        var spinner = ora('Start uploading ' + fileName)
+        spinner.start()
 
-    var uploadParams = {
-        s3Params: {
-            Bucket: bucketName,
-            ACL
-        }
-    }
-
-    var spinner = ora('Start uploading')
-    spinner.start()
-
-    if (isDir) {
-        uploadParams.localDir = file
-
-        if (cwd && cwd !== '') {
-            uploadParams.s3Params.Prefix = cwd
+        var uploadParams = {
+            s3Params: {
+                Bucket: bucketName,
+                ACL
+            }
         }
 
-        var uploader = client.uploadDir(uploadParams);
-        uploader.on('error', function(err) {
-            console.error("unable to sync:", err.stack)
-            spinner.stop()
-        })
-        uploader.on('end', function() {
-            spinner.stop()
-            console.log("Deployment done")
-        })    
-    } else {
-        uploadParams.localFile = file
-        uploadParams.s3Params.Key = cwd + fileName
+        var s3Key = cwd
+        if (fromDirectory) {
+            s3Key += fromDirectory
+        }
+        s3Key += fileName
+
+        uploadParams.s3Params.Key = s3Key
+        uploadParams.localFile = filepath
 
         var uploader = client.uploadFile(uploadParams);
         uploader.on('error', function(err) {
-            console.error("unable to download:", err.stack);
             spinner.stop()
+            return reject(err.stack)
         })
         uploader.on('end', function() {
             spinner.stop()
-            console.log("Deployment done")
+            console.log(`Uploaded ${fileName} to ${s3Key.replace(fileName, '')}`)
+            return resolve()
+        })
+    })
+}
+
+/**
+ * List all the files inside a directory
+ * 
+ * @param {String} dir 
+ * @param {*} filelist 
+ */
+const walkSync = (dir, filelist = []) => {
+    fs.readdirSync(dir).forEach(file => {
+        filelist = fs.statSync(path.join(dir, file)).isDirectory()
+            ? walkSync(path.join(dir, file), filelist)
+            : filelist.concat(path.join(dir, file))
+    })
+
+    return filelist
+}
+
+/**
+ * 
+ * @param {Array<String>} files 
+ * @param {Promise} promise 
+ * @returns {Promise}
+ */
+const processUploadDir = (files, promise) => {
+    if (!files) {
+        return Promise.resolve({done: true})
+    }
+
+    if (!promise) {
+        let filePath = files.shift()
+        if (filePath) {
+            let fromDirectoryPath = filePath.replace(baseFilePath + '/', '').replace(path.basename(filePath), '')
+
+            return processUploadDir(files, startUpload(filePath, fromDirectoryPath))
+        } else {
+            return Promise.resolve({done: true})
+        }
+        
+    } else {
+        return promise
+        .catch(error => {
+            return Promise.reject(error)
+        })
+        .then(() => {
+            if (files.length > 0) {
+                let filePath = files.shift()
+                let fromDirectoryPath = filePath.replace(baseFilePath + '/', '').replace(path.basename(filePath), '')
+
+                return processUploadDir(files, startUpload(filePath, fromDirectoryPath))
+            } else {
+                return Promise.resolve({done: true})
+            }
         })
     }
 }
